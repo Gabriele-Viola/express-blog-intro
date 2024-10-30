a40c50c0020013f5870aedb75ffdfa33b6b9d35e
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-30 14:42:46 +0100

    add h1

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /*
 Esercizio
 Creiamo il nostro blog personale e giorno dopo giorno lo potremo arricchire con nuove funzionalità.
-Creiamo il progetto base con una rotta / che ritorna un h1 con scritto Benvenuto nel mio blog!
+Creiamo il progetto base con una rotta / che ritorna un h1 con scritto Benvenuto nel mio blog! ✅
 Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
 Creiamo poi una rotta /posts che restituisca un oggetto json con la lista dei post e il conteggio, partendo da un array locale.
 La rotta relativa ai post dovrà chiamare la funzione index() dal controller dedicato ( controllers/posts.js )
@@ -12,3 +12,16 @@ Spostiamo l’array dei post in un file separato da importare poi dentro il cont
 Creare una nuova rotta con cui stampare la lista in html come ul
 Create una pagina statica html da cui far partire una chiamata ajax per consumare il vostro enpoint json.
 */
+
+const express = require('express')
+const app = express()
+const port = 3000
+const host = 'http://127.0.0.1'
+
+app.get('/', (req, res) => {
+  res.send('<h1>Benvenuto nel mio blog</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${host}:${port}`)
+})
